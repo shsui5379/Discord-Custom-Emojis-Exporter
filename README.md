614e9807e41cb7e92f557b60f4af06c067532ae3
shsui5379 <shsui5379@users.noreply.github.com>
2021-01-31 16:45:25 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Discord-Custom-Emojis-Exporter
 Node.js script that exports custom emojis from servers using the Discord API
+
+Requires Node.js
+
+Instructions:
+1. Clone repo
+2. npm i
+3. Get an API token: https://discord.com/developers/applications
+4. Paste API token to line 8 of script.js
+5. Invite bot to servers (Requires you to have "Manage Server" permission)
+6. node script.js
+7. Check in the emojis directory
---
+++ script.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ ws: { intents: ['GUILDS'] } });
+const fs = require('fs');
+const https = require('https');
+
+fs.mkdirSync("emojis");
+
+client.login(' /* API token goes here: get one from here: https://discord.com/developers/applications */ ');
+
+client.on('ready', () => {
+    for (var server of client.guilds.cache) {
+        for (var emoji of server[1].emojis.cache) {
+            const url = emoji[1].url;
+            const file = fs.createWriteStream("./emojis/" + url.substring(url.lastIndexOf("/"), url.length));
+            https.get(url, function (response) {
+                response.pipe(file);
+            });
+        }
+    }
+});
